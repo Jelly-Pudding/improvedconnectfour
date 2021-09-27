db00aef83ce438bc222dc81cd78c672970d692b3
jelly-pudding <90060516+Jelly-Pudding@users.noreply.github.com>
2021-09-27 17:27:38 +0100

    add ai playing against itself as a feature

--- connect4.py
+++ connect4.py
@@ -1,10 +1,27 @@
 import copy
 import random
 import sys, os
+
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+sys.stdout = Unbuffered(sys.stdout)
+
+
+
 class ConnectFour():
 	turn = 0
 	evanorodd = 0
 	gameover = False
+	draw = False
 	xwin = 0
 	owin = 0
 	
@@ -116,12 +133,14 @@ class ConnectFour():
 		#checks if everywhere is full
 		spaces_left = sum(row.count(" ") for row in self.board)
 		if spaces_left == 0:
+			self.draw = True
 			self.gameover = True
 
 def blockprint():
 	sys.stdout = open(os.devnull, 'w')
 def enableprint():
 	sys.stdout = sys.__stdout__
+	sys.stdout = Unbuffered(sys.stdout)
 
 
 play = ConnectFour()
@@ -155,6 +174,7 @@ def evaluate_board(theclass):
 				num_top_o += 0.2
 		return num_top_x - num_top_o			
 def minimax(theclass, is_maximizing, depth, alpha, beta, evaluate_board):
+	blockprint()
 	classer = theclass
 	classer.checker()
 	if classer.gameover == True or depth == 0:
@@ -174,6 +194,7 @@ def minimax(theclass, is_maximizing, depth, alpha, beta, evaluate_board):
 			alpha = max(alpha, best_value)
 			if alpha >= beta:
 				break
+		enableprint()
 		return [best_value, best_move]
 	elif is_maximizing == False:
 		best_value = float("Inf")
@@ -190,102 +211,129 @@ def minimax(theclass, is_maximizing, depth, alpha, beta, evaluate_board):
 			beta = min(beta, best_move)
 			if alpha >= beta:
 				break
+		enableprint()
 		return [best_value, best_move] 
  
+twoai = input("Want to watch a game played by two AIs? Y or N?: ")
 
+while twoai.lower() != "y" and twoai.lower() != "n":
+	twoai = input("Please enter either Y for yes or N for no: ")
 
-ai = input("Would you like to play against an AI? Y or N?: ")
-
-while ai.lower() != "y" and ai.lower() != "n":
-	ai = input("Please enter either Y for yes or N for no: ")
-if ai.lower() == "n":
+if twoai.lower() == "y":
 	while play.evanorodd < 43:
 		play.printer()
-		try:
-			columnnumber = int(input("Enter a column number from one to seven: "))
-		except ValueError:
-			columnnumber = int(input("\nPlease give an integer!: "))
-		play.inputter(columnnumber)
-		play.checker()
-		if play.gameover == True:
-			play.printer()
-			break
-		if play.evanorodd == 42:
-			play.gameover = True
-			print("\nThe game is drawn!")
-			play.printer()
-			break
-elif ai.lower() == "y":
-	iffirst = input("Would you like to go first? Y or N?: ")
-	while iffirst.lower() != "y" and iffirst.lower() != "n":
-		iffirst = input("Please enter either Y for yes or N for no: ")
-	if iffirst.lower() == "y":	
+		if play.evanorodd % 2 == 0:
+			aimove = minimax(play, True, 6, -float("Inf"), float("Inf"), evaluate_board)[1]
+			play.aiinputter(aimove)
+			print("\nai one dropped a piece in column {column}.".format(column=int(aimove/2+0.5)))
+			play.checker()
+			if play.gameover == True and play.draw != True:
+				play.printer()
+				break
+			if play.evanorodd == 42:
+				print("\nThe game is drawn!")
+				play.printer()
+				break	
+		elif play.evanorodd % 2 != 0:
+			aimove = minimax(play, False, 6, -float("Inf"), float("Inf"), evaluate_board)[1]
+			play.aiinputter(aimove)
+			print("\nai two dropped a piece in column {column}.".format(column=int(aimove/2+0.5)))
+			play.checker()
+			if play.gameover == True and play.draw != True:
+				play.printer()
+				break
+			if play.evanorodd == 42:
+				print("\nThe game is drawn!")
+				play.printer()
+				break
+elif twoai.lower() == "n":
+	ai = input("Would you like to play against an AI? Y or N?: ")
+
+	while ai.lower() != "y" and ai.lower() != "n":
+		ai = input("Please enter either Y for yes or N for no: ")
+	if ai.lower() == "n":
 		while play.evanorodd < 43:
 			play.printer()
-			if play.evanorodd % 2 == 0:
-				try:
-					columnnumber = int(input("Enter a column number from one to seven: "))
-				except ValueError:
-					columnnumber = int(input("\nPlease give an integer!: "))
-				play.inputter(columnnumber)
-				play.checker()
-				if play.gameover == True:
-					play.printer()
-					break
-				if play.evanorodd == 42:
-					play.gameover = True
-					print("\nThe game is drawn!")
-					play.printer()
-					break
+			try:
+				columnnumber = int(input("Enter a column number from one to seven: "))
+			except ValueError:
+				columnnumber = int(input("\nPlease give an integer!: "))
+			play.inputter(columnnumber)
+			play.checker()
+			if play.gameover == True and play.draw != True:
+				play.printer()
+				break
+			if play.evanorodd == 42:
+				print("\nThe game is drawn!")
+				play.printer()
+				break
+	elif ai.lower() == "y":
+		iffirst = input("Would you like to go first? Y or N?: ")
+		while iffirst.lower() != "y" and iffirst.lower() != "n":
+			iffirst = input("Please enter either Y for yes or N for no: ")
+		if iffirst.lower() == "y":	
+			while play.evanorodd < 43:
+				play.printer()
+				if play.evanorodd % 2 == 0:
+					try:
+						columnnumber = int(input("Enter a column number from one to seven: "))
+					except ValueError:
+						columnnumber = int(input("\nPlease give an integer!: "))
+					play.inputter(columnnumber)
+					play.checker()
+					if play.gameover == True and play.draw != True:
+						play.printer()
+						break
+					if play.evanorodd == 42:
+						print("\nThe game is drawn!")
+						play.printer()
+						break
 			
-			elif play.evanorodd % 2 != 0:
-				blockprint()
-				aimove = minimax(play, False, 6, -float("Inf"), float("Inf"), evaluate_board)[1]
-				play.aiinputter(aimove)
-				enableprint()
-				print("\nThe ai dropped a piece in column {column}.".format(column=int(aimove/2+0.5)))
-				play.checker()
-				if play.gameover == True:
-					play.printer()
-					break
-				if play.evanorodd == 42:
-					play.gameover = True
-					print("\nThe game is drawn!")
-					play.printer()
-					break
-	if iffirst.lower() == "n": 	
-		while play.evanorodd < 43:
-			play.printer()
-			if play.evanorodd % 2 == 0:
-				blockprint()
-				aimove = minimax(play, True, 6, -float("Inf"), float("Inf"), evaluate_board)[1]
-				play.aiinputter(aimove)
-				enableprint()
-				print("\nThe ai dropped a piece in column {column}.".format(column=int(aimove/2+0.5)))
-				play.checker()
-				if play.gameover == True:
-					play.printer()
-					break
-				if play.evanorodd == 42:
-					play.gameover = True
-					print("\nThe game is drawn!")
-					play.printer()
-					break
-			elif play.evanorodd % 2 != 0:
-				try:
-					columnnumber = int(input("Enter a column number from one to seven: "))
-				except ValueError:
-					columnnumber = int(input("\nPlease give an integer!: "))
-				play.inputter(columnnumber)
-				play.checker()
-				if play.gameover == True:
-					play.printer()
-					break
-				if play.evanorodd == 42:
-					play.gameover = True
-					print("\nThe game is drawn!")
-					play.printer()
-					break				
+				elif play.evanorodd % 2 != 0:
+					blockprint()
+					aimove = minimax(play, False, 6, -float("Inf"), float("Inf"), evaluate_board)[1]
+					play.aiinputter(aimove)
+					enableprint()
+					print("\nThe ai dropped a piece in column {column}.".format(column=int(aimove/2+0.5)))
+					play.checker()
+					if play.gameover == True and play.draw != True:
+						play.printer()
+						break
+					if play.evanorodd == 42:
+						print("\nThe game is drawn!")
+						play.printer()
+						break
+		if iffirst.lower() == "n": 	
+			while play.evanorodd < 43:
+				play.printer()
+				if play.evanorodd % 2 == 0:
+					blockprint()
+					aimove = minimax(play, True, 6, -float("Inf"), float("Inf"), evaluate_board)[1]
+					play.aiinputter(aimove)
+					enableprint()
+					print("\nThe ai dropped a piece in column {column}.".format(column=int(aimove/2+0.5)))
+					play.checker()
+					if play.gameover == True and play.draw != True:
+						play.printer()
+						break
+					if play.evanorodd == 42:
+						print("\nThe game is drawn!")
+						play.printer()
+						break
+				elif play.evanorodd % 2 != 0:
+					try:
+						columnnumber = int(input("Enter a column number from one to seven: "))
+					except ValueError:
+						columnnumber = int(input("\nPlease give an integer!: "))
+					play.inputter(columnnumber)
+					play.checker()
+					if play.gameover == True and play.draw != True:
+						play.printer()
+						break
+					if play.evanorodd == 42:
+						print("\nThe game is drawn!")
+						play.printer()
+						break				
 				
 
 
